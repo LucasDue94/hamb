75b68b652601160aed60e28d9687249dfa02e869
Franklin Farias <franklin.farias@hospitaldocoracao-al.com.br>
2019-07-26 09:36:25 -0300

    Component Agenda-list final.

--- client/src/app/agenda/agenda-show/agenda-show.component.ts
+++ client/src/app/agenda/agenda-show/agenda-show.component.ts
@@ -7,13 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendaShowComponent implements OnInit {
 
-
   pacientes = [
-    {id:1, prontuario: 214589, codpac: 2458, nome: "José da Silva Peixoto", convenio: "Particular"},
-    {id:2, prontuario: 214590, codpac: 2557, nome: "Maria da Graça da Silva", convenio: "Particular"},
-    {id:3, prontuario: 214591, codpac: 2358, nome: "Manuela dos Santos da Silva", convenio: "Unimed"},
-    {id:4, prontuario: 214592, codpac: 2151, nome: "Márcio dos Santos da Silva", convenio: "Bradesco"},
-    {id:5, prontuario: 214593, codpac: 2052, nome: "Wilma Soares Melo", convenio: "Smile"}
+    {id:1, prontuario: 214589, codpac: 2458, nome: "José da Silva Peixoto", idade: 50, convenio: "Particular", retorno: true},
+    {id:2, prontuario: 214590, codpac: 2557, nome: "Maria da Graça da Silva", idade: 30, convenio: "Particular", retorno: false},
+    {id:3, prontuario: 214591, codpac: 2358, nome: "Manuela dos Santos da Silva", idade: 20, convenio: "Unimed", retorno: false },
+    {id:4, prontuario: 214592, codpac: 2151, nome: "Márcio dos Santos da Silva", idade: 38, convenio: "Bradesco", retorno: true},
+    {id:5, prontuario: 214593, codpac: 2052, nome: "Wilma Soares Melo", idade: 28, convenio: "Smile", retorno: false}
   ]
 
   constructor() { }
